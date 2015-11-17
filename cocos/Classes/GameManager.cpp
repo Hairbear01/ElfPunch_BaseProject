5e37f24a33dca7b0fd38a5bc992d34cd2dc1b214
Mike430 <ymike2045@gmail.com>
2015-11-17 20:24:21 +0000

    First draft of the game manager. All this will be is a class that will be passed from scene to scene containing core information, like score and settings

--- cocos/Classes/GameManager.cpp
+++ cocos/Classes/GameManager.cpp
@@ -3,9 +3,38 @@
 
 GameManager::GameManager()
 {
+	//Default Setting
+	_score = 0;
+	_numOfElfs = 50;
+	_speed = 30.0f;
 }
 
 
 GameManager::~GameManager()
 {
+}
+
+int GameManager::GetScore()
+{
+	return _score;
+}
+
+void GameManager::AddToScore(int increment)
+{
+	_score += increment;
+}
+
+void GameManager::ResetScore()
+{
+	_score = 0;
+}
+
+void GameManager::SetNumOfElfs(int numOfElfs)
+{
+	_numOfElfs = numOfElfs;
+}
+
+void GameManager::SetSpeed(float newSpeed)
+{
+	_speed = newSpeed;
 }
--- cocos/Classes/GameManager.h
+++ cocos/Classes/GameManager.h
@@ -1,8 +1,26 @@
 #pragma once
+#ifndef __GAMEMANAGER_H__
+#define __GAMEMANAGER_H__
+
+#include "cocos2d.h"
+
 class GameManager
 {
+private:
+	int _score;
+	int _numOfElfs;
+	float _speed;
 public:
 	GameManager();
 	~GameManager();
+
+	int GetScore();
+	void AddToScore(int increment);
+	void ResetScore();
+
+	void SetNumOfElfs(int numOfElfs);
+
+	void SetSpeed(float newSpeed);
 };
 
+#endif // __GAMEMANAGER_H__
